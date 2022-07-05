dae4f93451f795483841d6f91d02c6e133538c1f
athangk <athan.gkoumas@gmail.com>
2022-07-06 01:28:25 +0300

    Removes unecessary code

--- src/utilities/api-utils.ts
+++ src/utilities/api-utils.ts
@@ -23,6 +23,5 @@ export const normalizeCategoryData = (
   return categoryList;
 };
 
-export const sleep = (ms: number) => {
-  return new Promise((resolve) => setTimeout(resolve, ms));
-};
+export const sleep = (ms: number) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
